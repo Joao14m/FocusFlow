c77289f69af942474342238b2ff1eace7f963e25
Joao14m <joaomarsil4@gmail.com>
2024-11-25 20:20:28 -0500

    Changes in Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,33 @@
-// import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
 import Signup from './pages/Signup';
+// import Dashboard from './dashboard';
+import './styles/LogSign.css';
+// import Tasks from './Tasks';
+// import './styles/checkTask.css';
+import Homepage from './pages/Homepage';
+import './styles/homeP.css';
+// import './styles/menuIcon.css';
+// import './styles/sideMenu.css';
+// import './styles/homePG.css';
 
-function App() {
+// TypeScript types for the App component (optional but helpful for larger projects)
+const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Signup />} />
-        {/* Add other routes here, such as a login page */}
-        <Route path="/login" element={<div>Login Page (to be implemented)</div>} />
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Homepage" element={<Homepage />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          {/* <Route path="/Tasks" element={<Tasks />} /> */}
+        </Routes>
+      </div>
     </Router>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,17 @@
+// import React, {useState} from "react";
+// import { useNavigate } from "react-router-dom";
+
+const Homepage: React.FC = () => {
+
+    return (
+<div className="containerHome">
+    <div className="box" id="this-week">This Week</div> 
+    <div className="box" id="this-month">This Month</div>
+    <div className="box" id="todo-list">To-Do List</div>
+  </div>
+
+
+    );
+};
+
+export default Homepage;
