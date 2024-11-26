35eb6aa6e7d3d34a9f93cb49287ca5063a11a2f8
Joao14m <joaomarsil4@gmail.com>
2024-11-25 20:22:34 -0500

    Change in Homepage organization

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -2,15 +2,12 @@
 // import { useNavigate } from "react-router-dom";
 
 const Homepage: React.FC = () => {
-
     return (
-<div className="containerHome">
-    <div className="box" id="this-week">This Week</div> 
-    <div className="box" id="this-month">This Month</div>
-    <div className="box" id="todo-list">To-Do List</div>
-  </div>
-
-
+    <div className="containerHome">
+        <div className="box" id="this-week">This Week</div> 
+        <div className="box" id="this-month">This Month</div>
+        <div className="box" id="todo-list">To-Do List</div>
+    </div>
     );
 };
 
